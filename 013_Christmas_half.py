92c527b1c29af8148b97d2629d6bc8e2249150df
Litajong <58197187+Litajong@users.noreply.github.com>
2019-12-18 23:20:39 -0500

    Add files via upload

---
+++ 013_Christmas_half.py
@@ -0,0 +1,16 @@
+
+a = ""
+for k in range(4):
+    a = a + "*"
+    print(a)
+
+print("*@*@*@")
+
+for k in range(3):
+    a = a + "**"
+    print(a)
+  
+print("merry'x mas")  
+print("||||")
+print("||||")
+print("||||")
