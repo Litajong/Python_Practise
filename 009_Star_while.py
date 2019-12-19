0b0381e99a84e41f5b1c60e7df1b54cd0907ff8c
Litajong <58197187+Litajong@users.noreply.github.com>
2019-12-18 22:32:23 -0500

    Add files via upload

---
+++ 009_Star_while.py
@@ -0,0 +1,9 @@
+a = int(input("End Star Row:"))
+
+k = 1
+
+while k <= a :
+    i = "*" * k
+    print(i)
+    k += 1
+
---
+++ 010_Star_for.py
@@ -0,0 +1,4 @@
+b = int(input("End Star Row:"))
+for k in range (b) :
+    a = "*" * (k + 1)
+    print(a)
