73a049c2cb5f719e2835a76357d1e4055d8198be
Litajong <58197187+Litajong@users.noreply.github.com>
2019-12-18 23:20:10 -0500

    Add files via upload

---
+++ 014_MerryXmas.py
@@ -0,0 +1,16 @@
+a = "*"
+b = " "
+print((8*b) + (1 * a))
+print((7*b) + (2 * a) + (1 * a))
+print((6*b) + (3 * a) + (2 * a))
+print((5*b) + (4 * a) + (3 * a))
+print((7*b) + (2 * a) + (1 * a))
+print((6*b) + (3 * a) + (2 * a))
+print((5*b) + (4 * a) + (3 * a))
+print((4*b) + (5 * a) + (4 * a))
+print((3*b) + (6 * a) + (5 * a))
+print(" *@*@*@*@*@*@*@*")
+
+print("***merry'x mas***")  
+print("      |||||")
+print("      |||||")
